2fb37b11b68e366debee8642c13c244559667cf9
Zaidan Zulhakim <59697378+Zaidannzzz@users.noreply.github.com>
2021-11-17 10:54:05 +0700

    Merge branch 'main' of https://github.com/Zaidannzzz/simbah

--- resources/views/admin/homeadmin.blade.php
+++ resources/views/admin/homeadmin.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div>
-    <h1>admin</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptas nemo asperiores, officiis eveniet ut numquam esse, perspiciatis aliquam enim veniam cupiditate ipsam maiores, error fugit! Expedita assumenda sunt eligendi.</p>
+    
 </div>
 @endsection
